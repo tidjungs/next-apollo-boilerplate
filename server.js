1152748d65ccf4f60abef206542aeed9a21ca5db
tidjungs <waewprach.s@ku.th>
2017-09-25 11:29:51 +0700

    add server and routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const next = require('next');
+const routes = require('./routes');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = routes.getRequestHandler(app);
+app.prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
+  });

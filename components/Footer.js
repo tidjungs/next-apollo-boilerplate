f66706c172b9f61180e8e63f6daf95e7985ef993
komcal <kom.sujautra@gmail.com>
2017-10-11 13:57:25 +0700

    add layout with recompose

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <header>
+    <div>
+      Header
+    </div>
+  </header>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import withRedux from 'next-redux-wrapper';
-import { bindActionCreators } from 'redux';
-import styled from 'styled-components';
-import initStore from '../redux/store';
+import React from 'react';
+import withApp from '../lib/withApp';
+import LandingPage from '../containers/LandingPage';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`;
+const Index = props => (<LandingPage {...props} />);
 
-class Index extends Component {
-  render() {
-    return (
-      <div>
-        <Title>{this.props.test}</Title>
-      </div>
-    );
-  }
-}
-
-Index.propTypes = {
-  test: PropTypes.string,
-};
-
-const mapStateToProps = ({ index }) => ({
-  test: index.test,
-});
-
-const IndexWithRedux = withRedux(initStore, mapStateToProps, null)(Index);
-
-export default IndexWithRedux;
+export default withApp()(Index);

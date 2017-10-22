b8748b998f170273517ddb6fc6c4343f8cfa82a4
tidjungs <waewprach.s@ku.th>
2017-10-22 20:15:21 +0700

    compose container with page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { gql, graphql } from 'react-apollo';
+import PropTypes from 'prop-types';
+import { compose } from 'recompose';
 import withApp from '../lib/withApp';
-import LandingPage from '../containers/LandingPage';
 
-const Index = props => (<LandingPage {...props} />);
+const LandingPage = ({ data: { loading, error, todos } }) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  return (
+    <div>
+      <div>
+        {
+          todos.map(todo => (
+            <p key={todo.id}>{todo.text}</p>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
 
-export default withApp()(Index);
+LandingPage.propTypes = {
+  data: PropTypes.object,
+};
+
+export const todosListQuery = gql`
+  {
+    todos {
+      id
+      text
+    }
+  }
+`;
+
+export default compose(
+  withApp(),
+  graphql(todosListQuery, {
+    options: {},
+  }),
+)(LandingPage);
